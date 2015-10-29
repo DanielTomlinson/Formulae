9f328131134b92192d66c8277c3b419a1354f0f3
Danielle Lancashire <dani@builds.terrible.systems>
2015-10-29 00:09:01 +0000

    Add sha256 for git-cleanup

--- Formula/git-cleanup.rb
+++ Formula/git-cleanup.rb
@@ -2,6 +2,7 @@ class GitCleanup < Formula
     desc "A small utility for cleaning up merged git branches"
     homepage "https://github.com/endocrimes/git-cleanup"
     url "https://github.com/endocrimes/git-cleanup/archive/0.0.1.tar.gz"
+    sha256 "4859b395aec8f832521c73e3654dfe06cb2a38fbbe0a14fb4b289ed19de3e113"
 
     def install
         bin.install "git-cleanup"
